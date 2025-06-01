01f20c521e6942af0866fd28f4dcacb19af87725
7bdollz <ibehsuccess06@gmail.com>
2025-06-01 23:19:40 +0100

    I AM DONE WITH FIRST VERSION OF THE BABY TRANSLATOR

---
+++ script.js
@@ -0,0 +1,34 @@
+let inputField = document.querySelector("#userInput");
+let outputField = document.querySelector("#userOutput");
+let translateButton = document.querySelector("#uploadButton");
+
+let words = {
+  play: "pway",
+  daddy: "dada",
+  mum: "moma",
+  brother: "bubba",
+  sister: "sissy",
+  blankets: "blankie",
+  train: "choo choo",
+  dog: "goggy",
+  cat: "meow",
+  car: "go go",
+  pain: "owch",
+  delicious: "yummy",
+};
+let userInput = "";
+
+translateButton.addEventListener("click", () => {
+  userInput = inputField.value;
+  let splitInput = userInput.split(" ");
+  for (let i = 0; i < splitInput.length; i++) {
+    for (let key in words) {
+      if (splitInput[i] === key) {
+        splitInput[i] = words[key];
+      }
+    }
+  }
+  let output = splitInput.join(" ");
+  outputField.value = output;
+  inputField.value = "";
+});
